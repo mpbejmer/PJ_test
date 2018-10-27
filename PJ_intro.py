a1d60178eca2cefbf2b1981ecc9110b9f168b2ba
mpbejmer <mpbejmer@gmail.com>
2018-10-27 22:48:13 +0200

    Problem 2 solved

--- PJ_intro.py
+++ PJ_intro.py
@@ -1,7 +1,24 @@
-sum = 0
+#Problem 1
+sum1 = 0
 
 for x in range(0,1000):
   if (x%3 == 0) or (x%5 == 0):
-    sum+=x
+    sum1+=x
 
-print("Sum is equal to:", sum)
+print("Sum is equal to:", sum1)
+
+#Problem2
+
+sum2 = 2
+fib1 = 1
+fib2 = 2
+fib = 0
+while fib <= 4E+6:
+    fib = fib1 + fib2
+    if fib%2 == 0:
+        sum2+=fib
+
+    fib1 = fib2
+    fib2 = fib
+
+print("Fib sum is:", sum2)
