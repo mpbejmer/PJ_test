f9c1e83784d84dbccef7b6861fd0de29a8e7d64b
mpbejmer <mpbejmer@gmail.com>
2018-10-29 15:04:47 +0100

    Solved Problem 3

--- PJ_intro.py
+++ PJ_intro.py
@@ -1,3 +1,4 @@
+import numpy as np
 #Problem 1
 sum1 = 0
 
@@ -22,3 +23,41 @@ while fib <= 4E+6:
     fib2 = fib
 
 print("Fib sum is:", sum2)
+
+#Problem3
+
+def isPrime(n):
+    result  = True
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    i = 3
+    while (i*i) <= n + 5:
+        if n % i == 0:
+            result = False
+            return result
+        i = i + 2
+    return result
+
+def primefactor(n1):
+    n1_part = n1
+    maxprimefactor = 0
+    idx = 2
+    while idx <= n1_part // 2 + 1:
+        if isPrime(n1_part):
+            if n1_part > maxprimefactor:
+                maxprimefactor = n1_part
+                return maxprimefactor
+        if n1_part % idx == 0 and isPrime(idx):
+            n1_part = n1_part//idx
+            if idx > maxprimefactor:
+                maxprimefactor = idx
+            idx = 1
+        idx = idx + 1
+    return maxprimefactor
+
+nbr = 600851475143
+res = primefactor(nbr)
+print("Is ", nbr, " prime? ", isPrime(nbr))
+print("Greatest prime factor of ", nbr, " is: ", res)
